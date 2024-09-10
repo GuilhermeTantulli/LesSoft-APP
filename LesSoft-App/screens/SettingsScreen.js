06f732015df251671335e10019c42792a48ebcd3
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-09-10 09:52:39 -0300

    feat: SettingsScreen.js

--- LesSoft-App/screens/SettingsScreen.js
+++ LesSoft-App/screens/SettingsScreen.js
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; // Certifique-se de ter o @expo/vector-icons instalado
 
 const SettingsScreen = ({ navigation }) => {
@@ -17,15 +17,62 @@ const SettingsScreen = ({ navigation }) => {
     }
   };
 
+  const showAlert = (title, message) => {
+    Alert.alert(title, message);
+  };
+
   return (
     <View style={styles.container}>
-      <Ionicons
-        name="power-outline" // Ícone de logout
-        size={30}
-        color="#F03C25" // Cor vermelha #F03C25
-        style={styles.logoutIcon} // Estilo para posicionamento
-        onPress={handleLogout} // Chama a função de logout ao clicar
-      />
+      {/* Tarja no topo */}
+      <View style={styles.banner}>
+        <Image
+          source={require('../assets/LesSoft-logo-no-text.png')}
+          style={styles.icon}
+        />
+        <Text style={styles.bannerText}>Configurações</Text>
+      </View>
+
+      {/* Lista de botões */}
+      <View style={styles.buttonList}>
+        <TouchableOpacity 
+          style={styles.option}
+          onPress={() => showAlert('Versão', '1.0.0')}
+        >
+          <Text style={styles.optionText}>Versão</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={styles.option}
+          onPress={() => showAlert('Política de Privacidade', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')}
+        >
+          <Text style={styles.optionText}>Política de Privacidade</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={styles.option}
+          onPress={() => showAlert('Relatar Problema', 'Função não-implementada.')}
+        >
+          <Text style={styles.optionText}>Relatar Problema</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={styles.option}
+          onPress={() => showAlert('Central de Ajuda', 'Função não-implementada.')}
+        >
+          <Text style={styles.optionText}>Central de Ajuda</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Botão de logout */}
+      <View style={styles.logoutContainer}>
+        <Ionicons
+          name="power-outline"
+          size={30}
+          color="#F03C25"
+          style={styles.logoutIcon}
+          onPress={handleLogout}
+        />
+      </View>
     </View>
   );
 };
@@ -33,14 +80,47 @@ const SettingsScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#333',
   },
+  banner: {
+    width: '100%',
+    backgroundColor: '#222227', // Cor da tarja
+    alignItems: 'center',
+    paddingVertical: 20, // Ajusta a altura da tarja
+    marginTop: 30,
+    marginBottom: 120, // Espaço abaixo da tarja
+  },
+  bannerText: {
+    color: '#fff',
+    fontSize: 22,
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  icon: {
+    width: 14,
+    height: 13,
+  },
+  buttonList: {
+    marginBottom: 100, // Espaço para o botão de logout
+    marginHorizontal: 20, // Margem lateral para centralizar os botões
+  },
+  option: {
+    backgroundColor: '#444',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+  optionText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  logoutContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   logoutIcon: {
-    position: 'absolute',
-    top: 30,
-    right: 20, // Posicionado no canto superior direito
+    marginTop: -100,
   },
 });
 
